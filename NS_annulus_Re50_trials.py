3ac740cbfc4b3f1451c97869377a4ba83445d426
Becca Thomases <bthomases@smith.edu>
2026-04-10 16:15:15 -0400

    update parameters

--- NS_annulus_Re50_trials.py
+++ NS_annulus_Re50_trials.py
@@ -125,12 +125,12 @@ print(f"MPI hello from rank {rank} of {size}", flush=True)
 base_run_root = Path("/work/pi_bthomases_smith_edu/bthomases_smith_edu/runs/dedalus_NS_annulus")
 
 Re_target = 50.0
-Amp       = 0.01
+Amp       = 0.1
 
 t0 = 500.0
 t1 = 503.005
 
-manual_restart_file = "/work/pi_bthomases_smith_edu/bthomases_smith_edu/runs/dedalus_NS_annulus/Re50_Ap01/seg_t00200p00_to_t00500p01/checkpoints/checkpoints_s31.h5"
+manual_restart_file = "/work/pi_bthomases_smith_edu/bthomases_smith_edu/runs/dedalus_NS_annulus/Re50_Ap1/seg_t00200p00_to_t00500p01/checkpoints/checkpoints_s31.h5"
 restart_index = -1
 
 #t0 = 99.0
